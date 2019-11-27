825379a7ced6a1dc601beae4559b661635d2047b
spant786 <spant786@gmail.com>
2019-11-27 09:39:59 +0545

    Initial Start

---
+++ app/src/main/java/com/example/kitcheware_and_furniture_store/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.kitcheware_and_furniture_store;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
